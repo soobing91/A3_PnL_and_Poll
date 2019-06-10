326adc2fa801f0c008a2b6e159cf88414a4af60d
Soo Bin Hwang <soobing@Soos-MacBook-Pro.local>
2019-06-09 23:12:52 -0400

    Update before sleeping.. June 9, 2019

--- PyPoll/main_poll_SooBin.py
+++ PyPoll/main_poll_SooBin.py
@@ -3,21 +3,50 @@ import csv
 
 csvpath = os.path.join('..', 'election_data.csv')
 
-voterCount = 0
+total = 0
 
+votedFor = []
 candList = []
-candUnique = []
+results = {}
+
+maximum = 0
 
 with open(csvpath, 'r', newline = '') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     next(csvreader, None)
 
     for row in csvreader:
-        voterCount += 1
-        candList.append(row[2])
         
-for cand in candList:
-        if cand not in candList:
-            candUnique.append(cand)
+        # Total votes
+        total += 1
 
-print(candUnique)
+        # Storing candidates
+        votedFor.append(row[2])
+
+        # List of candidates
+        candidate = row[2]
+        if candidate not in candList:
+            candList.append(candidate)
+
+            # Setting up a counter for a candidate found
+            results[candidate] = 0
+
+        # Number of votes counted
+        results[candidate] += 1
+
+    print("Election Results")
+    print("-------------------------")
+    print(f"Total Votes: {total}")
+
+    # Calculating percentages
+    for cand in results:
+        numerator = results.get(cand)
+        percent = round(numerator / total, 2)
+        print(f"{cand}: {percent} ({numerator})")
+
+        if results > maximum:
+            maximum = results
+            winner = cand
+            print(cand)
+
+    print("-------------------------")
